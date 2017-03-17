282e86929b45dd82f77bdfbc15f2fb3cefdb9e5a
Jonathan Lancaster <jcast90@me.com>
2017-03-16 17:50:38 -0700

    add folders/images

---
+++ app5.js
@@ -0,0 +1,76 @@
+// 1.
+// function displayMessage(){
+// 		window.alert("I have been Clicked!");
+// 	};
+// 2.
+// var button = document.getElementById('clickButton');
+// button.onclick = function(){
+// 	window.alert("I have also been clicked!");
+// 	}
+// 3.	
+// button2.addEventListener("click", showMessage, false);
+// function showMessage(){
+// 	window.alert("I have been clicked because I am cooler!!!");
+// };
+// 4.
+// button2.addEventListener("click", function(){
+// 	window.alert("I have been clicked because I am cooler!!!");
+// }, false);
+// 5.
+// 	window.addEventListener("load", init, false);
+// function init(){
+// 		button2.addEventListener("click", showMessage, false);
+// function showMessage(){
+// 			window.alert("I have been clicked because I am cooler!!!");
+// 			}
+// 	};
+// 6.
+// redirect.addEventListener("click", displayMessage, false);
+// function displayMessage(){
+// 	event.preventDefault();
+// 	window.alert("You clicked on the link, but it aint gonna work!");
+// 	};
+// 7.
+// submit.addEventListener("click", function(){
+// 	submit.disabled = true;
+// 	window.alert(theText.value);
+// },false);
+// 8.
+// newPage.addEventListener("click", function(){
+// 	var newWindow = window.open('newpage.html','gg','width=300, height=300');
+// 		newWindow.resizeBy(500,300);	
+// },false);
+
+
+// 9.
+ 
+// var interval = setInterval(function(){ log() },2000);
+// 
+// start.addEventListener("click", log ,false);
+// 
+// function log(){	
+// 	console.log("Learning JavaScript is fun!!");
+// };
+// 
+// var stop = document.getElementById('stop');
+// stop.onclick = function(){
+// 	clearInterval(interval);
+// 	};
+
+// 10.
+
+// var button = document.getElementById('select_btn');
+// var option = document.getElementById('mySelect');
+// 
+// button.onclick = function(){
+// 	window.alert(option.value);
+// };
+
+
+
+
+
+
+
+
+
